b4d1df0ec1ed195bcaa601c30e6b2edf3627a755
Zach McGrath <zmcgrath96@gmail.com>
2019-02-05 14:37:58 -0600

    Draw line found on a pretty picture

--- server/astar.py
+++ server/astar.py
@@ -2,6 +2,11 @@ import random
 import numpy as np
 import cv2 as cv
 
+# OPEN CV CONSTANTS
+line_width = 3
+line_color = (0, 0, 255)
+file_name = 'pretty.png'
+
 class Node():
 	"""A node class for A* Pathfinding"""
 
@@ -105,14 +110,36 @@ def astar(maze, start, end):
 
 def main():
 
+	# GENERATE A MAZE WITH A PATH
 	n = 240
 	maze = [[0] * n for i in range(n)]
 
 	for i in range(0, len(maze)):
 		for j in range(len(maze[i])):
-			a = random.randint(1, 100)
+			a = random.randint(1, 10)
 			b = random.randint(1, 10)
 			maze[i][j] = 1 if a % b == 0 else 0
+
+	# try and make sure that every block isn't completely surrounded 
+	for i in range(len(maze)):
+		for j in range(len(maze[i])):
+			blocked = True
+			for next_pos in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]:
+				xo = 0
+				yo = 0
+				if i+next_pos[0] >= n:
+					xo = -1 * n
+				if j+next_pos[1] >= n:
+					yo = -1 *n
+				if maze[i+next_pos[0] + xo][j+next_pos[1] + yo] != 1:
+					blocked = False 
+					break 
+			if blocked:
+				a = random.randint(-1, 1)
+				b = random.randint(-1, 1)
+				maze[i+a][j+b] = 0
+
+	# END MAP GENERATE
 
 	start = (0, 0)
 	end = (n - 1, n - 1)
@@ -123,12 +150,11 @@ def main():
 	path = astar(maze, start, end)
 	
 	#print(path)
-	vis = np.zeros((384, 836), np.float32)
-	h,w = vis.shape
-	vis2 = cv.CreateMat(h, w, cv.CV_32FC3)
-	vis0 = cv.fromarray(vis)
-	cv.CvtColor(vis0, vis2, cv.CV_GRAY2BGR)
-	
+	img = np.zeros((len(maze), len(maze[0]), 3), np.uint8)
+	print(len(path))
+	for i in range(len(path)-1):
+		cv.line(img, path[i], path[i+1], line_color, line_width)
+	cv.imwrite(file_name, img)
 
 if __name__ == '__main__':
 	main()
