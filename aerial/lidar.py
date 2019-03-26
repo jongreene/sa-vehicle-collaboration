ed35d8fa2ffc7cffe0533feb2f68398f9ea953ad
root <root@localhost.localdomain>
2019-03-26 15:04:05 -0500

    LIDAR files added

---
+++ aerial/lidar.py
@@ -0,0 +1,47 @@
+##
+## Date: March 21, 2019
+## LIDAR python file for use with the Garmin Lidar Lite v3 sensor
+## Ellis Springe
+##
+
+# IMPORTS
+
+import sys
+import smbus
+import time
+
+# CONSTANTS
+
+dbus = 1
+daddr = 0x62
+
+bus = smbus.SMBus(dbus)
+
+HEIGHT = 10 # height in feet
+CONV = .3048 # FT to Meters conversion
+ALTITUDE = HEIGHT * CONV * 1000  # height in centimeters, in practice this should be read from altimeter
+
+
+#
+# Retrieve depth value from LIDAR I2C register
+#
+# Inputs: none
+# Returns: int (depth value)
+def get_depth():
+
+    num = 0
+
+    for i in range(0,4):
+        bus.write_byte_data(daddr,0x00,0x04)
+
+        flag = True
+
+        while ( flag ):
+            bi = "{0:b}".format(bus.read_byte_data(daddr,0x01))
+            if (bi[len(bi)-1] == '0'):
+                flag = False
+
+        val = bus.read_i2c_block_data(daddr,0x8f,2)
+        num += (val[0] * 256) + val[1]
+
+    return ALTITUDE - (num / 4) - 20
--- server/mapCSV.py
+++ server/mapCSV.py
@@ -9,47 +9,47 @@ def mapCSV(csv_file):
 	if not os.path.isfile(csv_file) or '.csv' not in str(csv_file):
 		print('Error: File either does not exist or is not a csv')
 		sys.exit()
-	
+
 	# generate a numpy array from the csv with type float
 	m1 = np.genfromtxt(csv_file, dtype=float, delimiter=',')
-	
-	# TODO: search and correct for missing csv values 
+
+	# TODO: search and correct for missing csv values
 
 	#return the x, y gradients
 	# [gx1, gy1] = np.gradient(m1)
-	
+
 	# print("\nGradient in the 'x direction':")
 	# print(gx1)
 	# print("\nGradient in the 'y direction':")
 	# print(gy1)
-	
+
 	# vehicleMetrics = {'maxPitchSlope': 2, 'maxRollSlope': 2}
-	
+
 	# constructAstarArray(gx1, gy1, vehicleMetrics)
 
 	return m1
-	
+
 def constructAstarArray(xGradientArray, yGradientArray, vehicleMetrics):
 	# maybe remove this line, doing this could lead to the vehicle
 	# attempting to go forward up a slope it could only traverse
 	# horizontally
 	# TODO: search for alternative solutions
 	#minSlope = min(vehicleMetrics['maxPitchSlope'], vehicleMetrics['maxRollSlope'])
-	
+
 	arr = np.int_([row[:] for row in xGradientArray])
-	
+
 	for i in range(0, len(xGradientArray)):
 		for j in range(0, len(xGradientArray[i])):
 			# here, 1 = not traversable, 0 = traversable
 			arr[i][j] = 1 if (abs(xGradientArray[i][j]) > vehicleMetrics['maxPitchSlope']) or (abs(yGradientArray[i][j]) > vehicleMetrics['maxPitchSlope']) else 0
-	
+
 	print("\nArray given to A*:")
 	print(arr)
-	
+
 	print("\nNote: 0 = traversable, 1 = not traversable, for visualization purposes")
 
 def main():
-	terrain = mapCSV('terrain.csv')
+	terrain = mapCSV("terrain.csv")
 
 	x = range(len(terrain))
 	y = range(len(terrain[0]))
