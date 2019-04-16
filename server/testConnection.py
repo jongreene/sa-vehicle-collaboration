59862f4b182e14a6e6adb1f5c5e3cf97e9d0cb50
Matthew Taylor <mjt@ku.edu>
2019-04-16 20:19:53 +0100

    can now send messages of any length over ble/uart

--- server/testConnection.py
+++ server/testConnection.py
@@ -1,107 +1,76 @@
-# Example of interaction with a BLE UART device using a UART service
-# implementation.
-# Author: Tony DiCola
 import Adafruit_BluefruitLE
 from Adafruit_BluefruitLE.services import UART
 import time
 
-
-# Get the BLE provider for the current platform.
 ble = Adafruit_BluefruitLE.get_provider()
 
-data = ['{turn, left, 0, 0}', '{drive, two_wheel, 50, 50}',  '{turn, right, 0, 0}', '{drive, four_wheel, 90, 0, 90, 0}']
-stop = '{stop, all}'
+data = ['{turn, left, 0, 0}', '{stop, all}', '{long-instruction-that-we-shouldn\'t-be-able-to-send, two_wheel, 50, 50}', '{stop, all}', '{turn, right, 0, 0}', '{stop, all}','{drive, four_wheel, 90, 0, 90, 0}', '{stop, all}']
 
-# Main function implements the program logic so it can run in a background
-# thread.  Most platforms require the main thread to handle GUI events and other
-# asyncronous events like BLE actions.  All of the threading logic is taken care
-# of automatically though and you just need to provide a main function that uses
-# the BLE provider.
 def main():
-    # Clear any cached data because both bluez and CoreBluetooth have issues with
-    # caching data and it going stale.
     ble.clear_cached_data()
-
-    # Get the first available BLE network adapter and make sure it's powered on.
     adapter = ble.get_default_adapter()
     adapter.power_on()
-    #print('Using adapter: {0}'.format(adapter.name))
-
-    # Disconnect any currently connected UART devices.  Good for cleaning up and
-    # starting from a fresh state.
-    #print('Disconnecting any connected UART devices...')
     UART.disconnect_devices()
 
-    # Scan for UART devices.
-    #print('Searching for UART device...')
     try:
         adapter.start_scan()
-        # Search for the first UART device found (will time out after 60 seconds
-        # but you can specify an optional timeout_sec parameter to change it).
         device = UART.find_device()
         if device is None:
             raise RuntimeError('Failed to find UART device!')
     finally:
-        # Make sure scanning is stopped before exiting.
         adapter.stop_scan()
 
-    #print('Connecting to device...')
-    device.connect()  # Will time out after 60 seconds, specify timeout_sec parameter
-                      # to change the timeout.
+    device.connect()
 
-    # Once connected do everything else in a try/finally to make sure the device
-    # is disconnected when done.
     try:
-        # Wait for service discovery to complete for the UART service.  Will
-        # time out after 60 seconds (specify timeout_sec parameter to override).
-        #print('Discovering services...')
         UART.discover(device)
-
-        # Once service discovery is complete create an instance of the service
-        # and start interacting with it.
         uart = UART(device)
 
         global data
         global stop
 
-        # Write a string to the TX characteristic.
-        #print("Sent 'Hello world!' to the device.")
-
-        # Now wait up to one minute to receive data from the device.
-        #print('Waiting up to 60 seconds to receive data from the device...')
-        #received = uart.read(timeout_sec=60)
-        #if received is not None:
-            # Received data, print it out.
-        #    print('Received: {0}'.format(received))
-        #else:
-            # Timeout waiting for data, None is returned.
-        #    print('Received no data!')
-
         for d in data:
-            data_string = str(d) + '\r\n'
-            print('Sending: ' + str(d))
-            uart.write(data_string.encode('UTF-8'))
-            time.sleep(10)
-            data_string = str(stop) + '\r\n'
-            print('Sending: ' + str(d))
-            uart.write(data_string.encode('UTF-8'))
-            time.sleep(10)
-
-        uart.write('end of data'.encode('UTF-8'))
+            safe_send(uart, d)
 
     finally:
-        # Make sure device is disconnected on exit.
         device.disconnect()
 
 
 def send():
-
-    # Initialize the BLE system.  MUST be called before other BLE calls!
     ble.initialize()
-
-    # Start the mainloop to process BLE events, and run the provided function in
-    # a background thread.  When the provided main function stops running, returns
-    # an integer status code, or throws an error the program will exit.
     ble.run_mainloop_with(main)
+
+def safe_send(uart, data):
+    if data[-2:] != '\r\n':
+        data = data + '\r\n'
+
+    # if data is too long
+    if len(data) > 16:
+        # split it up
+        data_substrings = list(chunkstring(data, 14))
+
+        # and send each piece
+        for string in data_substrings:
+            safe_send(uart, string)
+
+    # if data is short enough to fully send
+    else:
+        # send data
+        uart.write(data.encode('UTF-8'))
+
+        # while receiver nacks, retransmit
+        while get_response(uart) != 'ack':
+            uart.write(data.encode('UTF-8'))
+
+def get_response(uart, timeout_sec=30):
+    response = uart.read(timeout_sec)
+
+    while response[-1] != '\n':
+        response = response + uart.read(timeout_sec)
+
+    return response.rstrip('\n').rstrip('\r')
+
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
 
 send()
