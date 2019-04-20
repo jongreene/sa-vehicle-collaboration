229e70c34dda3ed2107036d3d712f97f68a41e0d
Matthew Taylor <mjt@ku.edu>
2019-04-20 16:26:16 +0100

    Connection file cleanup

--- server/connection.py
+++ server/connection.py
@@ -3,12 +3,30 @@
 # Author: Tony DiCola
 import Adafruit_BluefruitLE
 from Adafruit_BluefruitLE.services import UART
+import time
 
+uart = None
+data = None
 
 # Get the BLE provider for the current platform.
 ble = Adafruit_BluefruitLE.get_provider()
 
-data = None
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
+def send_cmd(string):
+    global uart
+    substrings = list(chunkstring(string, 16))
+    substrings.append('\n')
+    for string in substrings:
+        uart.write(string.encode('UTF-8'))
+    received = uart.read(timeout_sec=60)
+    while received[-1] != '\n':
+        received = received + uart.read(timeout_sec=60)
+    if received is not None:
+        print(received[:-1])
+    else:
+        print('No data received')
 
 # Main function implements the program logic so it can run in a background
 # thread.  Most platforms require the main thread to handle GUI events and other
@@ -23,15 +41,15 @@ def main():
     # Get the first available BLE network adapter and make sure it's powered on.
     adapter = ble.get_default_adapter()
     adapter.power_on()
-    #print('Using adapter: {0}'.format(adapter.name))
+    print('Using adapter: {0}'.format(adapter.name))
 
     # Disconnect any currently connected UART devices.  Good for cleaning up and
     # starting from a fresh state.
-    #print('Disconnecting any connected UART devices...')
+    print('Disconnecting any connected UART devices...')
     UART.disconnect_devices()
 
     # Scan for UART devices.
-    #print('Searching for UART device...')
+    print('Searching for UART device...')
     try:
         adapter.start_scan()
         # Search for the first UART device found (will time out after 60 seconds
@@ -43,7 +61,7 @@ def main():
         # Make sure scanning is stopped before exiting.
         adapter.stop_scan()
 
-    #print('Connecting to device...')
+    print('Connecting to device...')
     device.connect()  # Will time out after 60 seconds, specify timeout_sec parameter
                       # to change the timeout.
 
@@ -52,49 +70,33 @@ def main():
     try:
         # Wait for service discovery to complete for the UART service.  Will
         # time out after 60 seconds (specify timeout_sec parameter to override).
-        #print('Discovering services...')
+        print('Discovering services...')
         UART.discover(device)
 
         # Once service discovery is complete create an instance of the service
         # and start interacting with it.
+        global uart
         uart = UART(device)
 
-        global data
+        # global data
+        # SEND DATA HERE
 
         # Write a string to the TX characteristic.
-        #print("Sent 'Hello world!' to the device.")
+        while True:
+            s = input()
+            if s == 's':
+                s = '{drive,two_wheel,0,0}'
+            elif len(s.split(' ')) == 2:
+                s = '{drive,two_wheel,' + s.split(' ')[0] + ',' + s.split(' ')[1] + '}'
+            elif len(s.split(' ')) == 4 and s.split(' ')[0] == 't':
+                s1 = '{drive,two_wheel,' + s.split(' ')[1] + ',' + s.split(' ')[2] + '}'
+                send(s1)
+                time.sleep(float(s.split(' ')[3]))
+                s = '{drive,two_wheel,0,0}'
+            elif len(s.split(' ')) == 4:
+                s = '{drive,four_wheel,' + s.split(' ')[0] + ',' + s.split(' ')[1] + ',' + s.split(' ')[2] + ',' + s.split(' ')[3] + '}'
 
-        # Now wait up to one minute to receive data from the device.
-        #print('Waiting up to 60 seconds to receive data from the device...')
-        #received = uart.read(timeout_sec=60)
-        #if received is not None:
-            # Received data, print it out.
-        #    print('Received: {0}'.format(received))
-        #else:
-            # Timeout waiting for data, None is returned.
-        #    print('Received no data!')
-
-        for d in data:
-            data_string = str(d) + '\r\n'
-            print('Sending: ' + str(d))
-            uart.write(data_string.encode('UTF-8'))
-
-            response = 0
-
-            while response == 0:
-                received = uart.read(timeout_sec=30)
-
-                while received[-1] != '\n':
-                    received = received + uart.read(timeout_sec=30)
-
-                print('Received:', received.rstrip('\r').rstrip('\n'))
-
-                if received and received.rstrip('\r').rstrip('\n') == 'ack':
-                    response = 1
-                else:
-                    uart.write(data_string.encode('UTF-8'))
-
-        uart.write('end of data'.encode('UTF-8'))
+            send_cmd(s)
 
     finally:
         # Make sure device is disconnected on exit.
@@ -102,10 +104,8 @@ def main():
 
 
 def send(incoming_data):
-
     global data
     data = incoming_data
-
     # Initialize the BLE system.  MUST be called before other BLE calls!
     ble.initialize()
 
@@ -114,3 +114,7 @@ def send(incoming_data):
     # an integer status code, or throws an error the program will exit.
     ble.run_mainloop_with(main)
 
+
+if __name__ == '__main__':
+    ble.initialize()
+    ble.run_mainloop_with(main)
